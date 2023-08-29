c5e28534b287afef0cef6c2f90ad4f558ad084be
pavigitup <pavithragokali@gmail.com>
2023-08-29 10:25:44 +0530

    update 3.0

--- src/component/event-pages/EventCont2.jsx
+++ src/component/event-pages/EventCont2.jsx
@@ -25,13 +25,228 @@ export const EventCon2 = () => {
             <button className='btn-blue'>ChenCon 2022 Proceedings</button>
         </div>
         <div className="line"></div>
+        <img className='event-img' src="/img/bacgroung/chencon2022.jpeg" alt="img" />
+        <div className="line"></div>
          <div className="event2-con1-scope">
+       
          <div className="event2-con1-orange">
             Scope of the Conference
         </div>
         <div className="event2-con1-gray-1">
         Recently, the use of AI/ML technology has been exponentially increasing in our daily lives. AI/ML applications represent an important evolution in computer science and data processing that is quickly transforming a vast array of domains. IETE CHENCON 2022 provides vibrant opportunities for the delegates to exchange new ideas, applications, practical challenges encountered and to leverage AI and ML technologies. The technical program of the conference will consist of invited key note talks and paper presentations. IETE CHENCON 2022 accepts technical paper presentation from research scholars, faculty, students and industry professionals in emerging areas of AI and ML applications but not limited to:
         </div>
+         </div>
+         <div className="line"></div>
+         <div className="event2-con1-ulcon">
+            <div className="ul-head">
+              Track - 1: AI Enabled Technologies
+            </div>
+              <div className="event2-con1-ulcon-ul">
+                  <ul>
+                      <li>&#x2714; Advanced Robotics</li>
+                      <li>&#x2714; Intelligent Drones and UAVs</li>
+                      <li>&#x2714; Natural Language Processing</li>
+                      <li>&#x2714; Speech Recognition</li>
+                      <li>&#x2714; Image Processing</li>
+                  </ul>
+
+                  <ul>
+                      <li>&#x2714; Conversational Systems</li>
+                      <li>&#x2714; Image Analysis</li>
+                      <li>&#x2714; Computer Vision</li>
+                      <li>&#x2714; Face and Gesture Recognition</li>
+                      <li>&#x2714; Object Recognition</li>
+                  </ul>
+
+                  <ul>
+                      <li>&#x2714; Document Analysis</li>
+                      <li>&#x2714; Earth Observation and Visualization</li>
+                      <li>&#x2714; Weather and Climate Prediction</li>
+                      <li>&#x2714; Localization Techniques</li>
+                      <li>&#x2714; Ethical and Responsible AI</li>
+                  </ul>
+                </div>
+         </div>
+         <div className="event2-con1-ulcon">
+            <div className="ul-head">
+              Track - 1: AI Enabled Technologies
+            </div>
+              <div className="event2-con1-ulcon-ul">
+                  <ul>
+                      <li>&#x2714; Advanced Robotics</li>
+                      <li>&#x2714; Intelligent Drones and UAVs</li>
+                      <li>&#x2714; Natural Language Processing</li>
+                      <li>&#x2714; Speech Recognition</li>
+                      <li>&#x2714; Image Processing</li>
+                  </ul>
+
+                  <ul>
+                      <li>&#x2714; Conversational Systems</li>
+                      <li>&#x2714; Image Analysis</li>
+                      <li>&#x2714; Computer Vision</li>
+                      <li>&#x2714; Face and Gesture Recognition</li>
+                      <li>&#x2714; Object Recognition</li>
+                  </ul>
+
+                  <ul>
+                      <li>&#x2714; Document Analysis</li>
+                      <li>&#x2714; Earth Observation and Visualization</li>
+                      <li>&#x2714; Weather and Climate Prediction</li>
+                      <li>&#x2714; Localization Techniques</li>
+                      <li>&#x2714; Ethical and Responsible AI</li>
+                  </ul>
+                </div>
+         </div>
+         <div className="event2-con1-ulcon">
+            <div className="ul-head">
+              Track - 1: AI Enabled Technologies
+            </div>
+              <div className="event2-con1-ulcon-ul">
+                  <ul>
+                      <li>&#x2714; Advanced Robotics</li>
+                      <li>&#x2714; Intelligent Drones and UAVs</li>
+                      <li>&#x2714; Natural Language Processing</li>
+                      <li>&#x2714; Speech Recognition</li>
+                      <li>&#x2714; Image Processing</li>
+                  </ul>
+
+                  <ul>
+                      <li>&#x2714; Conversational Systems</li>
+                      <li>&#x2714; Image Analysis</li>
+                      <li>&#x2714; Computer Vision</li>
+                      <li>&#x2714; Face and Gesture Recognition</li>
+                      <li>&#x2714; Object Recognition</li>
+                  </ul>
+
+                  <ul>
+                      <li>&#x2714; Document Analysis</li>
+                      <li>&#x2714; Earth Observation and Visualization</li>
+                      <li>&#x2714; Weather and Climate Prediction</li>
+                      <li>&#x2714; Localization Techniques</li>
+                      <li>&#x2714; Ethical and Responsible AI</li>
+                  </ul>
+                </div>
+         </div>
+         <div className="event2-con1-ulcon">
+            <div className="ul-head">
+              Track - 1: AI Enabled Technologies
+            </div>
+              <div className="event2-con1-ulcon-ul">
+                  <ul>
+                      <li>&#x2714; Advanced Robotics</li>
+                      <li>&#x2714; Intelligent Drones and UAVs</li>
+                      <li>&#x2714; Natural Language Processing</li>
+                      <li>&#x2714; Speech Recognition</li>
+                      <li>&#x2714; Image Processing</li>
+                  </ul>
+
+                  <ul>
+                      <li>&#x2714; Conversational Systems</li>
+                      <li>&#x2714; Image Analysis</li>
+                      <li>&#x2714; Computer Vision</li>
+                      <li>&#x2714; Face and Gesture Recognition</li>
+                      <li>&#x2714; Object Recognition</li>
+                  </ul>
+
+                  <ul>
+                      <li>&#x2714; Document Analysis</li>
+                      <li>&#x2714; Earth Observation and Visualization</li>
+                      <li>&#x2714; Weather and Climate Prediction</li>
+                      <li>&#x2714; Localization Techniques</li>
+                      <li>&#x2714; Ethical and Responsible AI</li>
+                  </ul>
+                </div>
+         </div>
+         <div className="line"></div>
+         <div className="event2-con1-scope">
+       
+         <div className="event2-con1-orange">
+            Scope of the Conference
+        </div>
+        <div className="event2-con1-gray-1">
+        Recently, the use of AI/ML technology has been exponentially increasing in our daily lives. AI/ML applications represent an important evolution in computer science and data processing that is quickly transforming a vast array of domains. IETE CHENCON 2022 provides vibrant opportunities for the delegates to exchange new ideas, applications, practical challenges encountered and to leverage AI and ML technologies. The technical program of the conference will consist of invited key note talks and paper presentations. IETE CHENCON 2022 accepts technical paper presentation from research scholars, faculty, students and industry professionals in emerging areas of AI and ML applications but not limited to:
+        </div>
+         </div>
+         <div className="line"></div>
+    
+         <div className="event2-con1-scope">
+       
+         <div className="event2-con1-orange">
+            Scope of the Conference
+        </div>
+         </div>
+         <div className="line"></div>
+         <div className="event2-con1-table">
+            <table>
+                <tr>
+                  <th>Registration Fee & Details</th>
+                  <th>Important Dates</th>
+                </tr>
+                <tr>
+                <td>
+                    <div>
+                        At least one author for each paper should register for the conference.<br/>
+                        If an author has more than one accepted paper,<br/>
+                        registration fee has to be paid for each Paper ID.<br/><br/>
+                        IETE members including ISF : Rs. 1500 + GST<br/>
+                        NON IETE participants : Rs. 2000 + GST<br/>
+                        International Participants : $50
+                    </div>
+                  </td>
+                  <td>
+                        Last date for paper Submission : 06.05.2022<br/>
+                        Notification of Acceptance : 10.05.2022<br/>
+                        Registration on or before : 15.05.2022<br/>
+                        Conference Date : 20.05.2022 & 21.05.2022
+                  </td>
+                </tr>
+            </table>
+         </div>
+         <div className="event2-con1-table">
+            <table>
+                <tr>
+                  <td>
+                      <div>
+                          Patron
+                          Prof Wg Cdr P. Prabhakar
+
+                          Organizing Chair
+                          Dr. N. Venkateswaran
+
+                          Conference Co-Chairs
+                          Mrs. T.J. Jeyaprabha
+                          Mr. Arun
+                      </div>
+                  </td>
+                  <td rowSpan={0}>
+                    <div >
+                        Advisory Committee
+                        Dr. J.W. Bakal
+                        Dr. K.T.V Reddy
+                        Dr. Surendra Pal
+                        Prof. K.K. Aggarwal
+                        Dr. M.H. Kori
+                        Dr. Shiv Kumar
+                        Dr. K. Jaya Shankar
+                        Col. (Retd) K.S. Chakravarthi
+                        Shri. S. Ramaswamy
+                        Dr. Swarna Ravindra Babu
+                        Dr. Tata Sudhakar
+                    </div>
+                      
+                  </td>
+                </tr>
+                <tr>
+                  <td >
+                  <div>
+                          Mobile: 72000 96048
+                          Phone: 044-28350773
+                          Email: chencon2021@gmail.com
+                          Website: http://ietechennai.in
+                      </div>
+                  </td>
+                </tr>
+            </table>
          </div>
      </div>
      

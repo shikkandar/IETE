0687f40a456bbc59ecb871f3d20b5aef3af79c59
pavigitup <pavithragokali@gmail.com>
2023-08-30 18:19:38 +0530

    Small changes

--- src/component/commitee/Commitee.jsx
+++ src/component/commitee/Commitee.jsx
@@ -14,7 +14,7 @@ export const Commitee = () => {
         <div className="program-cards-con-2">
           <div className="program-cards-2" data-aos="flip-right">
           <div className="p-c-img-2">
-            <img className="explore-images-2" src="/img/commitee-profile/profile.png" alt="Undergraduate Programs" />
+            <img className="explore-images-2" src="/img/commitee-profile/venkat.png" alt="Undergraduate Programs" />
           </div>
           <div className="program-line-2"></div>
           <div className="p-c-info-2">
@@ -34,21 +34,19 @@ export const Commitee = () => {
           </div>
           <div className="program-cards-2" data-aos="flip-right">
           <div className="p-c-img-2">
-            <img className="explore-images-2" src="/img/commitee-profile/profile.png" alt="Undergraduate Programs" />
+            <img className="explore-images-2" src="/img/commitee-profile/tata_sudhakar.jpeg" alt="Undergraduate Programs" />
           </div>
           <div className="program-line-2"></div>
           <div className="p-c-info-2">
             <div className="info-head-2">
-              Dr.N.Venkateswaran
+               Dr. Tata Sudhakar
             </div>
             <div className="info-content-1">
-              Chairman
+                Immd Post Chairman
             </div>
-            <div className="info-content-2">
-              Professor,Dept. of ECE Sri Siva Subramaniya Nadar College Of Engineering
-            </div>
+          
             <div className="info-content-3">
-              venkateswaran.iete@gmail.com
+            tatasudhakar@gmail.com
             </div>
           </div>
           </div>
@@ -59,16 +57,38 @@ export const Commitee = () => {
           <div className="program-line-2"></div>
           <div className="p-c-info-2">
             <div className="info-head-2">
-              Dr.N.Venkateswaran
+                Mr. Shakthi Murugan
             </div>
             <div className="info-content-1">
-              Chairman
+                Honarary Secretary
             </div>
             <div className="info-content-2">
-              Professor,Dept. of ECE Sri Siva Subramaniya Nadar College Of Engineering
+                 Assistant Professor / ECE
+                Jeppiaar Maamallan Engineering College
             </div>
             <div className="info-content-3">
-              venkateswaran.iete@gmail.com
+            shakthimuruganece@gmail.com
+            </div>
+          </div>
+          </div>
+          <div className="program-cards-2" data-aos="flip-right">
+          <div className="p-c-img-2">
+            <img className="explore-images-2" src="/img/commitee-profile/jeyaprabha.jpg" alt="Undergraduate Programs" />
+          </div>
+          <div className="program-line-2"></div>
+          <div className="p-c-info-2">
+            <div className="info-head-2">
+            Mrs T J Jeyaprabha
+            </div>
+            <div className="info-content-1">
+            Honarary Treasurer
+            </div>
+            <div className="info-content-2">
+                 Assistant Professor / ECE
+                  Sri Venkateswara College of Engineering
+            </div>
+            <div className="info-content-3">
+                  jprabha@svce.ac.in
             </div>
           </div>
           </div>
@@ -79,16 +99,13 @@ export const Commitee = () => {
           <div className="program-line-2"></div>
           <div className="p-c-info-2">
             <div className="info-head-2">
-              Dr.N.Venkateswaran
+            Dr C Udayakumar
             </div>
             <div className="info-content-1">
-              Chairman
-            </div>
-            <div className="info-content-2">
-              Professor,Dept. of ECE Sri Siva Subramaniya Nadar College Of Engineering
+            EC Member
             </div>
             <div className="info-content-3">
-              venkateswaran.iete@gmail.com
+            cukumar@gmail.com
             </div>
           </div>
           </div>
@@ -99,16 +116,32 @@ export const Commitee = () => {
           <div className="program-line-2"></div>
           <div className="p-c-info-2">
             <div className="info-head-2">
-              Dr.N.Venkateswaran
+            Dr V Thulasibai
             </div>
             <div className="info-content-1">
-              Chairman
+            EC Member
             </div>
-            <div className="info-content-2">
-              Professor,Dept. of ECE Sri Siva Subramaniya Nadar College Of Engineering
-            </div>
+        
             <div className="info-content-3">
-              venkateswaran.iete@gmail.com
+            thulasi9840@gmail.com
+            </div>
+          </div>
+          </div>
+          <div className="program-cards-2" data-aos="flip-right">
+          <div className="p-c-img-2">
+            <img className="explore-images-2" src="/img/commitee-profile/Vijendrababu.jpg" alt="Undergraduate Programs" />
+          </div>
+          <div className="program-line-2"></div>
+          <div className="p-c-info-2">
+            <div className="info-head-2">
+            Mr D Vijendrababu
+            </div>
+            <div className="info-content-1">
+            EC Member
+            </div>
+          
+            <div className="info-content-3">
+            dvijendrababu@gmail.com
             </div>
           </div>
           </div>
@@ -119,16 +152,32 @@ export const Commitee = () => {
           <div className="program-line-2"></div>
           <div className="p-c-info-2">
             <div className="info-head-2">
-              Dr.N.Venkateswaran
+            Shri Ganapathy Suresh
             </div>
             <div className="info-content-1">
-              Chairman
+            EC Member
             </div>
-            <div className="info-content-2">
-              Professor,Dept. of ECE Sri Siva Subramaniya Nadar College Of Engineering
-            </div>
+            
             <div className="info-content-3">
-              venkateswaran.iete@gmail.com
+                lovechennai@yahoo.com
+            </div>
+          </div>
+          </div>
+          <div className="program-cards-2" data-aos="flip-right">
+          <div className="p-c-img-2">
+            <img className="explore-images-2" src="/img/commitee-profile/arunkumar.jpeg" alt="Undergraduate Programs" />
+          </div>
+          <div className="program-line-2"></div>
+          <div className="p-c-info-2">
+            <div className="info-head-2">
+            Shri S Arun Kumar
+            </div>
+            <div className="info-content-1">
+            EC Member
+            </div>
+            
+            <div className="info-content-3">
+            arunsundar.s@cvrde.drdo.in
             </div>
           </div>
           </div>
@@ -139,136 +188,68 @@ export const Commitee = () => {
           <div className="program-line-2"></div>
           <div className="p-c-info-2">
             <div className="info-head-2">
-              Dr.N.Venkateswaran
+            Shri S Madivaanan
             </div>
             <div className="info-content-1">
-              Chairman
+            EC Member
             </div>
-            <div className="info-content-2">
-              Professor,Dept. of ECE Sri Siva Subramaniya Nadar College Of Engineering
-            </div>
+    
             <div className="info-content-3">
-              venkateswaran.iete@gmail.com
+            madivaanan.s@gmail.com
             </div>
           </div>
           </div>
           <div className="program-cards-2" data-aos="flip-right">
           <div className="p-c-img-2">
-            <img className="explore-images-2" src="/img/commitee-profile/profile.png" alt="Undergraduate Programs" />
+            <img className="explore-images-2" src="/img/commitee-profile/dr_vaishali.jpg" alt="Undergraduate Programs" />
           </div>
           <div className="program-line-2"></div>
           <div className="p-c-info-2">
             <div className="info-head-2">
-              Dr.N.Venkateswaran
+            Dr Vaishali D
             </div>
             <div className="info-content-1">
-              Chairman
+            Co-opted Member
             </div>
-            <div className="info-content-2">
-              Professor,Dept. of ECE Sri Siva Subramaniya Nadar College Of Engineering
-            </div>
+     
             <div className="info-content-3">
-              venkateswaran.iete@gmail.com
+            vaishali.b@vdp.srmuniv.ac.in
             </div>
           </div>
           </div>
           <div className="program-cards-2" data-aos="flip-right">
           <div className="p-c-img-2">
-            <img className="explore-images-2" src="/img/commitee-profile/profile.png" alt="Undergraduate Programs" />
+            <img className="explore-images-2" src="/img/commitee-profile/arun.jpeg" alt="Undergraduate Programs" />
           </div>
           <div className="program-line-2"></div>
           <div className="p-c-info-2">
             <div className="info-head-2">
-              Dr.N.Venkateswaran
+            Mr Arun
             </div>
             <div className="info-content-1">
-              Chairman
+            Co-opted Member
             </div>
-            <div className="info-content-2">
-              Professor,Dept. of ECE Sri Siva Subramaniya Nadar College Of Engineering
-            </div>
+            
             <div className="info-content-3">
-              venkateswaran.iete@gmail.com
+            snatarajan1967@gmail.com
             </div>
           </div>
           </div>
           <div className="program-cards-2" data-aos="flip-right">
           <div className="p-c-img-2">
-            <img className="explore-images-2" src="/img/commitee-profile/profile.png" alt="Undergraduate Programs" />
+            <img className="explore-images-2" src="/img/commitee-profile/sumathi.jpeg" alt="Undergraduate Programs" />
           </div>
           <div className="program-line-2"></div>
           <div className="p-c-info-2">
             <div className="info-head-2">
-              Dr.N.Venkateswaran
+            Dr. K. Sumathi
             </div>
             <div className="info-content-1">
-              Chairman
+            Co-opted Member
             </div>
-            <div className="info-content-2">
-              Professor,Dept. of ECE Sri Siva Subramaniya Nadar College Of Engineering
-            </div>
+            
             <div className="info-content-3">
-              venkateswaran.iete@gmail.com
-            </div>
-          </div>
-          </div>
-          <div className="program-cards-2" data-aos="flip-right">
-          <div className="p-c-img-2">
-            <img className="explore-images-2" src="/img/commitee-profile/profile.png" alt="Undergraduate Programs" />
-          </div>
-          <div className="program-line-2"></div>
-          <div className="p-c-info-2">
-            <div className="info-head-2">
-              Dr.N.Venkateswaran
-            </div>
-            <div className="info-content-1">
-              Chairman
-            </div>
-            <div className="info-content-2">
-              Professor,Dept. of ECE Sri Siva Subramaniya Nadar College Of Engineering
-            </div>
-            <div className="info-content-3">
-              venkateswaran.iete@gmail.com
-            </div>
-          </div>
-          </div>
-          <div className="program-cards-2" data-aos="flip-right">
-          <div className="p-c-img-2">
-            <img className="explore-images-2" src="/img/commitee-profile/profile.png" alt="Undergraduate Programs" />
-          </div>
-          <div className="program-line-2"></div>
-          <div className="p-c-info-2">
-            <div className="info-head-2">
-              Dr.N.Venkateswaran
-            </div>
-            <div className="info-content-1">
-              Chairman
-            </div>
-            <div className="info-content-2">
-              Professor,Dept. of ECE Sri Siva Subramaniya Nadar College Of Engineering
-            </div>
-            <div className="info-content-3">
-              venkateswaran.iete@gmail.com
-            </div>
-          </div>
-          </div>
-          <div className="program-cards-2" data-aos="flip-right">
-          <div className="p-c-img-2">
-            <img className="explore-images-2" src="/img/commitee-profile/profile.png" alt="Undergraduate Programs" />
-          </div>
-          <div className="program-line-2"></div>
-          <div className="p-c-info-2">
-            <div className="info-head-2">
-              Dr.N.Venkateswaran
-            </div>
-            <div className="info-content-1">
-              Chairman
-            </div>
-            <div className="info-content-2">
-              Professor,Dept. of ECE Sri Siva Subramaniya Nadar College Of Engineering
-            </div>
-            <div className="info-content-3">
-              venkateswaran.iete@gmail.com
+            sumathi.ece@sairam.edu.in
             </div>
           </div>
           </div>
--- src/component/event-pages/EventCont2.jsx
+++ src/component/event-pages/EventCont2.jsx
@@ -69,91 +69,98 @@ export const EventCon2 = () => {
          </div>
          <div className="event2-con1-ulcon" data-aos='zoom-in-up'>
             <div className="ul-head" data-aos='zoom-in-up'>
-              Track - 1: AI Enabled Technologies
+                Track - 2: AI/ML Infrastructure
             </div>
               <div className="event2-con1-ulcon-ul">
                   <ul>
-                      <li>&#x2714; Advanced Robotics</li>
-                      <li>&#x2714; Intelligent Drones and UAVs</li>
-                      <li>&#x2714; Natural Language Processing</li>
-                      <li>&#x2714; Speech Recognition</li>
-                      <li>&#x2714; Image Processing</li>
+                      <li>&#x2714; Enhanced data visualization</li>
+                      <li>&#x2714; Data preparation</li>
+                      <li>&#x2714; Data labeling</li>
+                      <li>&#x2714; Big data</li>
+                      <li>&#x2714; Edge devices</li>
+                      
+                  </ul>
+                  <ul>
+                      <li>&#x2714; Cloud ML platform</li>
+                      <li>&#x2714; Digital health and well being</li>
+                      <li>&#x2714; Prediction/ Detection of diseases</li>
+                      <li>&#x2714; AI for Effective treatment/ therapy</li>
+                      <li>&#x2714; 3D imaging and sensing</li>
+                      
+                  </ul>
+                  <ul>
+                     <li>&#x2714; ML/AI for autonomous systems</li>
+                      <li>&#x2714; AI and ML for Healthcare</li>
+                      <li>&#x2714; AI and ML Pandemic tracking</li>
+                      <li>&#x2714; AI and ML for Virtual learning</li>
+                      <li>&#x2714; Physics inspired ML</li>
+                  </ul>
+
+                </div>
+         </div>
+         <div className="event2-con1-ulcon" data-aos='zoom-in-up'>
+            <div className="ul-head">
+            Track - 3: Emerging Applications of Al in Communication
+            </div>
+              <div className="event2-con1-ulcon-ul">
+                    <ul>
+                        <li>&#x2714; AI/ML in the PHY Layer</li>
+                        <li>&#x2714; AI and machine learning for 5G</li>
+                        <li>&#x2714; Propagation at mm Waves and THertz</li>
+                        <li>&#x2714; Beyond 5G & 6G communications</li>
+                        <li>&#x2714; New modulation and coding techniques</li>
+                       
+                    </ul>
+
+
+                  <ul>
+                        <li>&#x2714; Antenna array processing</li>
+                        <li>&#x2714; AI/ML for wireless/optical/satellite networks</li>
+                        <li>&#x2714; Reconfigurable Intelligent Surfaces</li>
+                        <li>&#x2714; Detection and Localization</li>
+                        <li>&#x2714; Radio Network Traffic Prediction</li>
+                       
                   </ul>
 
                   <ul>
-                      <li>&#x2714; Conversational Systems</li>
-                      <li>&#x2714; Image Analysis</li>
-                      <li>&#x2714; Computer Vision</li>
-                      <li>&#x2714; Face and Gesture Recognition</li>
-                      <li>&#x2714; Object Recognition</li>
-                  </ul>
-
-                  <ul>
-                      <li>&#x2714; Document Analysis</li>
-                      <li>&#x2714; Earth Observation and Visualization</li>
-                      <li>&#x2714; Weather and Climate Prediction</li>
-                      <li>&#x2714; Localization Techniques</li>
-                      <li>&#x2714; Ethical and Responsible AI</li>
+                        <li>&#x2714; Mobility Analytics</li>
+                        <li>&#x2714; Smart Communications</li>
+                        <li>&#x2714; MIMO and Multi-antenna Beamforming</li>
+                        <li>&#x2714; Microwaves and Antenna</li>
+                        <li>&#x2714; THz Sensing and Spectrum</li>
                   </ul>
                 </div>
          </div>
          <div className="event2-con1-ulcon" data-aos='zoom-in-up'>
             <div className="ul-head">
-              Track - 1: AI Enabled Technologies
+                  Track - 4: Industry Applications of AI
             </div>
               <div className="event2-con1-ulcon-ul">
+                    <ul>
+                        <li>&#x2714; Factory automation</li>
+                        <li>&#x2714; Autonomous driving and V2X</li>
+                        <li>&#x2714; Internet of Things Analytics</li>
+                        <li>&#x2714; Cryptography</li>
+                        <li>&#x2714; Energy Management and Analytics</li>
+
+                    </ul>
+
+
                   <ul>
-                      <li>&#x2714; Advanced Robotics</li>
-                      <li>&#x2714; Intelligent Drones and UAVs</li>
-                      <li>&#x2714; Natural Language Processing</li>
-                      <li>&#x2714; Speech Recognition</li>
-                      <li>&#x2714; Image Processing</li>
+                        <li>&#x2714; Smart farming environments</li>
+                        <li>&#x2714; Quantum Computing</li>
+                        <li>&#x2714; Cybersecurity</li>
+                        <li>&#x2714; Cryptocurrencies</li>
+                        <li>&#x2714; Smart Cities Based on loT and AI</li>
+                       
                   </ul>
 
                   <ul>
-                      <li>&#x2714; Conversational Systems</li>
-                      <li>&#x2714; Image Analysis</li>
-                      <li>&#x2714; Computer Vision</li>
-                      <li>&#x2714; Face and Gesture Recognition</li>
-                      <li>&#x2714; Object Recognition</li>
-                  </ul>
-
-                  <ul>
-                      <li>&#x2714; Document Analysis</li>
-                      <li>&#x2714; Earth Observation and Visualization</li>
-                      <li>&#x2714; Weather and Climate Prediction</li>
-                      <li>&#x2714; Localization Techniques</li>
-                      <li>&#x2714; Ethical and Responsible AI</li>
-                  </ul>
-                </div>
-         </div>
-         <div className="event2-con1-ulcon" data-aos='zoom-in-up'>
-            <div className="ul-head">
-              Track - 1: AI Enabled Technologies
-            </div>
-              <div className="event2-con1-ulcon-ul">
-                  <ul>
-                      <li>&#x2714; Advanced Robotics</li>
-                      <li>&#x2714; Intelligent Drones and UAVs</li>
-                      <li>&#x2714; Natural Language Processing</li>
-                      <li>&#x2714; Speech Recognition</li>
-                      <li>&#x2714; Image Processing</li>
-                  </ul>
-
-                  <ul>
-                      <li>&#x2714; Conversational Systems</li>
-                      <li>&#x2714; Image Analysis</li>
-                      <li>&#x2714; Computer Vision</li>
-                      <li>&#x2714; Face and Gesture Recognition</li>
-                      <li>&#x2714; Object Recognition</li>
-                  </ul>
-
-                  <ul>
-                      <li>&#x2714; Document Analysis</li>
-                      <li>&#x2714; Earth Observation and Visualization</li>
-                      <li>&#x2714; Weather and Climate Prediction</li>
-                      <li>&#x2714; Localization Techniques</li>
-                      <li>&#x2714; Ethical and Responsible AI</li>
+                        <li>&#x2714; Industry 4.0</li>
+                        <li>&#x2714; Underwater Technologies</li>
+                        <li>&#x2714; 6G, Augmented /Virtual Reality</li>
+                        <li>&#x2714; VLSI designs of AI implementations</li>
+                        <li>&#x2714; AI/ML Chipsets</li>
                   </ul>
                 </div>
          </div>
@@ -161,18 +168,27 @@ export const EventCon2 = () => {
          <div className="event2-con1-scope" data-aos='zoom-in-up'>
        
          <div className="event2-con1-orange">
-            Scope of the Conference
+              Submission Guidelines
         </div>
         <div className="event2-con1-gray-1">
-        Recently, the use of AI/ML technology has been exponentially increasing in our daily lives. AI/ML applications represent an important evolution in computer science and data processing that is quickly transforming a vast array of domains. IETE CHENCON 2022 provides vibrant opportunities for the delegates to exchange new ideas, applications, practical challenges encountered and to leverage AI and ML technologies. The technical program of the conference will consist of invited key note talks and paper presentations. IETE CHENCON 2022 accepts technical paper presentation from research scholars, faculty, students and industry professionals in emerging areas of AI and ML applications but not limited to:
+                    Prospective authors are invited to submit 
+                    full papers describing original, previously unpublished, complete work in four pages in 
+                    A4 IEEE double column conference template, including figure, results and references. Submitted papers
+                    will be reviewed by two to three technical committee or reviewers.
+                    Accepted and registered papers of IETE CHENCON 2022 will be published in the conference proceedings.
+                    Extended versions of the accepted and presented papers will be recommended
+                    for publication in IETE Journals, SCOPUS, SCI and other indexed journals.
+                    Authors must submit the papers through their Easy chair account using the following link: <a className='event2-con1-blue' href='https://easychair.org/conferences/?conf=ietechencon2022'>https://easychair.org/conferences/?conf=ietechencon2022</a> or submit the papers to
+                    the Email ID: <span className='event2-con1-blue'>chencon2022@gmail.com</span>
+
         </div>
          </div>
          <div className="line" data-aos='zoom-in-up'></div>
     
          <div className="event2-con1-scope" data-aos='zoom-in-up'>
        
-         <div className="event2-con1-orange" data-aos='zoom-in-up'>
-            Scope of the Conference
+         <div className="event2-con1-orange-text" data-aos='zoom-in-up'>
+         Selected and presented papers will be published in the Journal of Science and Technology (ISSN 2456–5660)
         </div>
          </div>
          <div className="line" data-aos='zoom-in-up'></div>
@@ -207,30 +223,32 @@ export const EventCon2 = () => {
                 <tr>
                   <td>
                       <div>
-                          Patron
+                          <br/>
+                          <h4>Patron</h4>
                           Prof Wg Cdr P. Prabhakar
-
-                          Organizing Chair
+                          <br/><br/>
+                          <h4>Organizing Chair</h4>
                           Dr. N. Venkateswaran
-
-                          Conference Co-Chairs
+                          <br/><br/>
+                          <h4>Conference Co-Chairs</h4>
                           Mrs. T.J. Jeyaprabha
                           Mr. Arun
+                          <br/>
                       </div>
                   </td>
                   <td rowSpan={0}>
                     <div >
-                        Advisory Committee
-                        Dr. J.W. Bakal
-                        Dr. K.T.V Reddy
-                        Dr. Surendra Pal
-                        Prof. K.K. Aggarwal
-                        Dr. M.H. Kori
-                        Dr. Shiv Kumar
-                        Dr. K. Jaya Shankar
-                        Col. (Retd) K.S. Chakravarthi
-                        Shri. S. Ramaswamy
-                        Dr. Swarna Ravindra Babu
+                        <h4>Advisory Committee</h4>
+                        Dr. J.W. Bakal<br/>
+                        Dr. K.T.V Reddy<br/>
+                        Dr. Surendra Pal<br/>
+                        Prof. K.K. Aggarwal<br/>
+                        Dr. M.H. Kori<br/>
+                        Dr. Shiv Kumar<br/>
+                        Dr. K. Jaya Shankar<br/>
+                        Col. (Retd) K.S. Chakravarthi<br/>
+                        Shri. S. Ramaswamy<br/>
+                        Dr. Swarna Ravindra Babu<br/>
                         Dr. Tata Sudhakar
                     </div>
                       
@@ -239,10 +257,10 @@ export const EventCon2 = () => {
                 <tr>
                   <td >
                   <div>
-                          Mobile: 72000 96048
-                          Phone: 044-28350773
-                          Email: chencon2021@gmail.com
-                          Website: http://ietechennai.in
+                          Mobile: 72000 96048<br/>
+                          Phone: 044-28350773<br/>
+                          Email: <span className='event2-con1-blue'>chencon2021@gmail.com</span><br/>
+                          Website: <a className='event2-con1-blue' href='http://ietechennai.in'>http://ietechennai.in</a>
                       </div>
                   </td>
                 </tr>
